e149350263765e1e14065f878130bb2ec73a9c27
Mariotaku Lee <mariotaku.lee@gmail.com>
2012-09-23 22:21:13 +0800

    removed overridden methods in HostAddressResolver.

--- src/twitter4j/HostAddressResolver.java
+++ src/twitter4j/HostAddressResolver.java
@@ -4,11 +4,6 @@ import java.io.IOException;
 
 public interface HostAddressResolver {
 
-	@Override
-	public int hashCode();
-
 	public String resolve(String host) throws IOException;
 
-	@Override
-	public String toString();
 }
